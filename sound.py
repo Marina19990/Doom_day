395c89b15b0e3471b81fdf669c1d0b43d70e7e15
Marina19990 <tubeyousx@gmail.com>
2025-06-17 16:50:16 +0200

    first_commit

---
+++ sound.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 01:44:38 2023
+
+@author: Axelle
+"""
+
+import pygame as pg 
+
+class Sound:
+    def __init__(self, game):
+        self.game =game 
+        pg.mixer.init()
+        self.path = 'assets/sound/'
+        self.shotgun= pg.mixer.Sound(self.path + 'shotgun.wav')
+        self.npc_pain= pg.mixer.Sound(self.path + 'npc_pain.wav')
+        self.npc_death= pg.mixer.Sound(self.path + 'npc_death.wav')
+        self.npc_attack= pg.mixer.Sound(self.path + 'npc_attack.wav')
+        self.player_pain= pg.mixer.Sound(self.path + 'player_pain.wav')
+        self.theme= pg.mixer.Sound(self.path + 'theme.mp3')
